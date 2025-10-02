b91402678b73ba520b5a52607a22d6d5903d1bd5
Selvi5656 <selvipipirahma@gmail.com>
2025-10-02 09:18:56 +0700

    Add files via upload

---
+++ Hello World.dart
@@ -0,0 +1,16 @@
+void main() {
+   print('Hello, World!');
+    }
+
+
+    void main() {
+  // Komentar satu baris//
+  /* Komentar
+     lebih dari satu baris */
+  print('Belajar Dart');
+}
+
+void main() {
+  print("Hello, World!"); // kutip ganda juga valid
+}
+
